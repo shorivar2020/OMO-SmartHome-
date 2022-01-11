144a156c31d9a6cec88b590cc8f976540e233ddf
shorivar <shorivar@fel.cvut.cz>
2022-01-11 14:11:36 +0100

    Make Transport

--- Transport/Bicycle.java
+++ Transport/Bicycle.java
@@ -1,7 +1,23 @@
 package Transport;
 
 public class Bicycle extends Transport{
+    private int documentation = 0;
+    private int functionality = 100;
+    boolean deviceState = true;
+    static int count = 0;
     public Bicycle(){
         transport = "bicycle";
+        count++;
+        this.documentation = documentation;
+        this.functionality = functionality - count;
+    }
+
+    public boolean use(){
+        if(this.functionality != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- Transport/Car.java
+++ Transport/Car.java
@@ -1,7 +1,23 @@
 package Transport;
 
 public class Car extends Transport{
+    private int documentation = 0;
+    private int functionality = 100;
+    boolean deviceState = true;
+    static int count = 0;
     public Car(){
         transport = "Car";
+        count++;
+        this.documentation = documentation;
+        this.functionality = functionality - count;
+    }
+
+    public boolean use(){
+        if(this.functionality != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
