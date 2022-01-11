e0c9e46a48d6f6789235bd90c20dbb00d4d46d38
shorivar <shorivar@fel.cvut.cz>
2022-01-11 07:10:34 +0100

    Make Main

--- API/DATETIME.java
+++ API/DATETIME.java
@@ -1,4 +1,32 @@
-public class DATETIME(String Temperature, int clock) {
+import omo-smart-home.Event.HotTempeture;
+import java.util.Random;
+public class DATETIME(String temperature, int clock) {
+    public ArrayList<String> EventList = new ArrayList<String>();
+    if(temperature == "HOT"){
+        //HotTempeture();
+        EventList.add("HotTempeture");
+    }
+    else{
+        //ColdTemperature();
+        EventList.add("ColdTempeture");
+    }
+    if(clock == "DAY"){
+        switch (rand.nextInt(1)){
+            case 0:
+                //Chil();
+                EventList.add("Chill");
+                break;
+            case 1:
+                //Work();
+                EventList.add("Work");
+                break;
+        }
+    }
+    else{
+        //Night();
+        this.EventList.add("Night");
+    }
+    return (EventList);
     //if Hot -> Event HotTemperature
     //If Cold -> Event ColdTemperature
     //if night -> Event Night + chill
--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(){
+        //private List<Event> EventList = new ArrayList<>();
+        System.out.println("AAA");
+        DATETIME day;
+        for(int i = 0; i<=10; i++){
+            switch (rand.nextInt(3)){
+                case 0:
+                    day = new DATETIME("HOT", "DAY");
+                    System.out.println("HD");
+                    break;
+                case 1:
+                    day = new DATETIME("HOT", "NIGHT");
+                    System.out.println("HN");
+                    break;
+                case 2:
+                    day = new DATETIME("COLD", "DAY");
+                    System.out.println("CD");
+                    break;
+                case 3:
+                    day = new DATETIME("COLD", "NIGHT");
+                    System.out.println("CN");
+                    break;
+            }
+            List ListEvent = day.EventList();
+            System.out.println("The List is "+ListEvent);
+        }
         //Make house + take DATETIME
         //DO Event
     }
