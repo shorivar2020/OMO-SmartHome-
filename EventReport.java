49e8b6544eca54f98290dfd8ebeae2dbe74f68f5
shorivar <shorivar@fel.cvut.cz>
2022-01-18 08:35:09 +0100

    Make Event Report

---
+++ EventReport.java
@@ -0,0 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class EventReport {
+    public void EventReport(ArrayList<String> events) throws IOException {
+        FileWriter writEvent = new FileWriter("EventReport.txt", false);
+    }
+}
